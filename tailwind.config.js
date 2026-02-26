2eed2bb260a7b0eeb79d2a383184355110ba09ba
anantjindal1 <anantjindal1@gmail.com>
2026-02-26 19:28:31 +0530

    day 7 - personal dashboard with clock, weather, notes

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./day7-dashboard/index.html",
+    "./day7-dashboard/src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: { extend: {} },
+  plugins: [],
+}
